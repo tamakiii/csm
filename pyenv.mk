88fbf95ea362d56becb267c578c8f61e3eff3144
tamakiii <tamakiii@users.noreply.github.com>
2025-08-12 16:08:09 +0900

    ● Done! Now the pyenv.mk file is cleaner and focused on just   managing the virtual environment. The workflow is:
      1. make -f pyenv.mk setup - Creates the virtual environment
      2. make -f pyenv.mk activate - Activates the virtual
      environment
      3. make install - Installs dependencies (using the main
      Makefile)
      4. make -f pyenv.mk teardown - Removes the virtual
      environment when needed

--- pyenv.mk
+++ pyenv.mk
@@ -1,4 +1,4 @@
-.PHONY: help setup teardown activate install
+.PHONY: help setup teardown activate
 
 help:
 	@(cat $(firstword $(MAKEIFLE_LIST)))
@@ -15,13 +15,3 @@ activate: .venv
 .venv:
 	python -m venv .venv
 	.venv/bin/pip install --upgrade pip setuptools wheel
-
-install: .venv
-	@echo "Installing dependencies..."
-	-.venv/bin/pip install torch==2.6.0 torchaudio==2.6.0
-	-.venv/bin/pip install tokenizers==0.21.0 transformers==4.49.0 huggingface_hub==0.28.1
-	-.venv/bin/pip install moshi==0.2.2
-	-.venv/bin/pip install torchao==0.9.0
-	-.venv/bin/pip install git+https://github.com/SesameAILabs/silentcipher@master
-	@echo "Skipping torchtune due to sentencepiece dependency issue with Python 3.13"
-	@echo "Installation complete (some packages may have been skipped)"
